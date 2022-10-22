ed4021e34d95082f6d225ee402b8b7ee2413e2d3
timopix <57803428+timopix@users.noreply.github.com>
2022-10-22 11:58:45 +0600

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.pecuniaprojectmk1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onButtonCLick(View view) {
+        TextView txtHello = findViewById(R.id.textView);
+        txtHello.setText("Heys, Pecunia is alive!");
     }
 }
